ba788a0c91b05795ae7be2e52c687f12dccfe241
tamer bilici <tamerbilici00@gmail.com>
2023-08-27 18:22:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+import React from 'react';
+import Home from './Home.jsx';
+
+
+
+function App() {
+
+  return (
+    <>
+      <Home /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+
+    return (
+
+        <div>
+            <input class="form-control form-control-lg" type="text"
+                placeholder="Bir Görev Giriniz" aria-label=".form-control-lg example" />
+
+        </div>
+    )
+
+}

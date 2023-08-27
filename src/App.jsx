e9e9910e0270d8a5b5d617cc482dd9b5640d5717
tamer bilici <tamerbilici00@gmail.com>
2023-08-27 20:56:15 +0300

    form post

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import React from 'react';
 import Form from './Form.jsx';
 import Cards from './Cards.jsx';
+
+
 
 
 
@@ -10,8 +11,30 @@ import Cards from './Cards.jsx';
 // app function
 function App() {
 
+
+  // request to the server
+  const Request = async () => {
+
+    await fetch("http://localhost:8080/api/list")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+
+  }
+
+
+  //use effect create a function
+  useEffect(() => {
+    Request()
+  }, [])
+
+
+
+
   return (
     <>
+      <img src="src/assets/hamza.jpeg" className="card-img-top" alt="..." />
+    
       <Form />
       <Cards />
     </>
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Form() {
+
+
+
+    const [data, setData] = useState("")
+
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let res = await fetch("http://localhost:8080/api/create", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    task: data,
+                    completed: false,
+                    systemDate: "yeni tarih",
+                }),
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
+        setData("")
+    };
 
     return (
 
         <div className="card">
 
             <h1>ToDoo</h1>
-            <input type="email" autoComplete="off" class="form-control" id="exampleFormControlInput1"
+            <input value={data} onChange={(e) => setData(e.target.value)} type="email" autoComplete="off"
+                className="form-control" id="exampleFormControlInput1"
                 placeholder="You gotta add a few tasks first, you know?" />
-            <button type="button" class="mt-3 btn btn-primary">Add</button>
+            <button type="button" onClick={(e) => { handleSubmit(e) }} className="mt-3 btn btn-primary">Add</button>
 
         </div>
     )
